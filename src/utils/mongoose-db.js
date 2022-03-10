507bfb2731b7adcdd3a9ce3e2f31c98e5b779339
VRUJESH H M <60779508+VRU2306@users.noreply.github.com>
2022-03-10 19:18:40 +0530

    Delete mongoose-db.js

--- src/utils/mongoose-db.js
+++
@@ -1,9 +0,0 @@
-/* Mongodb database connection is done here*/ 
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://vrujesh_23:Vruje23@clusternew.phqfz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true}  , (err) => {
-    if (!err) { console.log('MongoDB Connection Succeeded.') }
-    else { console.log('Error in DB connection : ' + err) }
-});
-
-module.exports=mongoose;
